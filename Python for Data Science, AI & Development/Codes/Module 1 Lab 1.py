d6835f99ff991dad033881346e18973ecf1fa53e
Sagar-DU <nahidhasansagar@gmail.com>
2024-03-21 02:59:37 +0600

    started python

---
+++ Python for Data Science, AI & Development/Codes/Module 1 Lab 1.py
@@ -0,0 +1,69 @@
+# Say 'Hello' to the world in Python
+# print ("Bismillah Ar Rahmanir Rahim")
+
+# import sys
+
+# print (sys.version) # it shows the version of the installed Python
+
+# print ("This will run.")
+# frint ("This will cause error.")
+# print ("This will not run because of the previous error.")
+
+# Types of objects in Python
+# type (12)
+# print (type(12)) # the output will be int
+
+# type (2.14)
+# print (type(2.14)) # the output will be float
+
+# type ("Hello, world!")
+# print (type("Hello, world!")) # the output will be str
+
+# Converting from one object type to a different object type
+# type (2)
+# print (type(2)) #will give me an int
+
+# print (float(2)) #converted the int into float
+
+# print (int (1.1)) # converted the float into int and rounded to the lower integer value
+
+# print (int ("1")) # converted string 1 into int 1
+
+# print (int ("1 or 2 people")) # can't convert this one and will give me an error
+
+# print (float ("1.2")) # coverted into float
+
+# print (str(1)) #converted into string
+# print (type(str(1)))
+
+# print (str(1.2)) # converted float into string
+# print (type(str(1.2)))
+
+# Boolean Data type 
+# print (type(True))
+# print (type(False)) #These are the two types of booleans
+
+# print (int(True)) # will give me 1 as True
+# print (float(False)) # will give me 0.0 as False
+
+# Expressions and Variables 
+# print (43 + 60 + 16 + 41) # will give me the sum
+# print (50 - 60) # will give me substraction
+# print (5 * 5) # will give me multiplication
+# print (25/6) # will give me division and the result will always be in float
+# print (26//6) # will give me integer division
+
+# print (30 + 2 * 60) # will follow the mathematical rule
+# print ((30 + 2) * 60) # will do the brackets first
+
+# Variables
+# x = 43 + 60 + 16 + 41
+# print (x) # will give me the sum
+
+# y = x / 60
+# print (y) # can be use to define other variable in expression
+
+# x = x / 60
+# print (x) # can change itself
+
+# This all there is to in Lab 1!
