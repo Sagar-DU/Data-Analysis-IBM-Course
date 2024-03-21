ed5512263078c73d3a90cc6ba1eca2917fa0ceaa
Sagar-DU <nahidhasansagar@gmail.com>
2024-03-21 06:30:50 +0600

    learning python

---
+++ Python for Data Science, AI & Development/Codes/Module 1 Lab 2.py
@@ -0,0 +1,29 @@
+# String Operations
+
+# somethig written inside '' or "" is a string
+
+# print ("Nahid Hasan Sagar") # this is a string
+# print ("1 + 3 + 5 + 6") # this is also a string
+
+# firstName = "Nahid Hasan "
+# lastName = "Sagar"
+
+# fullName = firstName + lastName
+
+# print (firstName)
+# print (lastName)
+# print (fullName)
+
+# Indexing 
+# print (len(fullName))
+
+# print (fullName[0]) # will show me the first latter
+
+# print (fullName[16]) # will show me the last latter
+# print (fullName[-1]) # will also show me the last latter
+
+# Some String Methods
+# fullName = fullName.replace("Sagar", "")
+# print (fullName)
+
+# Incomplete
