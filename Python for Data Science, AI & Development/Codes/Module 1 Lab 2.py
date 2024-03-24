3686c9a4aac58f9f9972088044be7c95b87ed776
Sagar-DU <nahidhasansagar@gmail.com>
2024-03-24 12:28:51 +0600

    lab 2 excercise done

--- Python for Data Science, AI & Development/Codes/Module 1 Lab 2.py
+++ Python for Data Science, AI & Development/Codes/Module 1 Lab 2.py
@@ -113,4 +113,85 @@ s2 = "Michael Jackson was a singer and known as the 'King of Pop'"
 
 # print (new_string)
 
+# Excercise
+# 1. What is the value of the variable a after the following code is executed?
+a = "1"
+
+print (a)
+
+# 2. What is the value of the variable b after the following code is executed?
+b = "2"
+
+print (b)
+
+# 3. What is the value of the variable c after the following code is executed?
+c = a + b
+
+print (c)
+
+# 4. Consider the variable d use slicing to print out the first three elements:
+d = "ABCDEFG"
+
+print (d[:3])
+
+
+# 5. Use a stride value of 2 to print out every second character of the string e:
+e = 'clocrkr1e1c1t'
+
+print (e[::2])
+
+# 6. Print out a backslash:
+
+print ("\\")
+
+# 7. Convert the variable f to uppercase:
+f = "You are wrong"
+
+print (f.upper())
+
+# 8. Convert the variable f2 to lowercase:
+f2 = "YOU ARE RIGHT"
+
+print (f2.lower())
+
+# 9. Consider the variable g, and find the first index of the sub-string snow:
+g = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+
+print (g.find("snow"))
+
+# 10. In the variable g, replace the sub-string Mary with Bob:
+
+print (g.replace("Mary", "Bob"))
+
+# 11. In the variable g, replace the sub-string , with .:
+
+print (g.replace(",", "."))
+
+# 12. In the variable g, split the substring to list:
+
+print (g.split())
+
+# 13. In the string s3, find whether the digit is present or not using the \d and search() function:
+s3 = "House number- 1105"
+
+result = re.search ("\d", s3)
+
+if result:
+    print ("Digit found")
+else:
+    print ("Digit not found")
+
+# 14. In the string str1, replace the sub-string fox with bear using sub() function:
+str1= "The quick brown fox jumps over the lazy dog."
+
+print (re.sub("fox", "bear", str1, flags = re.IGNORECASE))
+
+# 15. In the string str2 find all the occurrences of woo using findall() function:
+str2= "How much wood would a woodchuck chuck, if a woodchuck could chuck wood?"
+
+print (re.findall("woo", str2))
+
+
 # Complete
