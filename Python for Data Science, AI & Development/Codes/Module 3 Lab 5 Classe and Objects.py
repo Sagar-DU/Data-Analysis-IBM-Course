a98f482160eb491abb060353b11264fabab9c4e7
Sagar-DU <nahidhasansagar@gmail.com>
2024-04-10 02:08:58 +0600

    Module 3 Lab 5 done!

--- Python for Data Science, AI & Development/Codes/Module 3 Lab 5 Classe and Objects.py
+++ Python for Data Science, AI & Development/Codes/Module 3 Lab 5 Classe and Objects.py
@@ -99,4 +99,59 @@ print("Width of the new Ractangle",FatYellowRectangle.width)
 print("Color of the new Ractangle",FatYellowRectangle.color)
 
 # Drawing the new Rectangle
-FatYellowRectangle.drawRectangle()
+# FatYellowRectangle.drawRectangle()
+
+# Excercise
+# Task-1. You are tasked with creating a Python program to represent vehicles using a class. Each car should have attributes for maximum speed and mileage.
+class Vehicle:
+    def __init__(self, maxSpeed, milage, color):
+        self.maxSpeed = maxSpeed
+        self.milage = milage
+
+# Task-2. Update the class with the default color for all vehicles," white".
+class Vehicle:
+    color = "white"
+    def __init__(self, maxSpeed, milage):
+        self.maxSpeed = maxSpeed
+        self.milage = milage
+
+# Task-3. Additionally, you need to create methods in the Vehicle class to assign seating capacity to a vehicle.
+class Vehicle:
+    color = "white"
+    def __init__(self, maxSpeed, milage):
+        self.maxSpeed = maxSpeed
+        self.milage = milage
+    
+    # Method
+    def assign_seatting_capacity (self, seating_capacity):
+        self.seating_capacity = seating_capacity
+
+# Task-4. Create a method to display all the properties of an object of the class.
+class Vehicle:
+    color = "white"
+    def __init__(self, maxSpeed, milage):
+        self.maxSpeed = maxSpeed
+        self.milage = milage
+    
+    # Method
+    def assign_seatting_capacity (self, seating_capacity):
+        self.seating_capacity = seating_capacity
+    
+    # Method
+    def display_properties (self):
+        print ("Properties of the Vehicle:")
+        print ("Color", self.color)
+        print ("Max Speed", self.maxSpeed)
+        print ("Milage", self.milage)
+        print ("Seating Capacity", self.seating_capacity)
+
+# Task-5. Additionally, you need to create two objects of the Vehicle class object that should have a max speed of 200kmph and mileage of 20kmpl with five seating capacities, and another car object should have a max speed of 180kmph and mileage of 25kmpl with four seating capacities.
+car1 = Vehicle(200, 20)
+car1.assign_seatting_capacity(5)
+print (car1.display_properties())
+
+car2 = Vehicle (180, 25)
+car2.assign_seatting_capacity(4)
+print (car2.display_properties())
+
+# Completed
