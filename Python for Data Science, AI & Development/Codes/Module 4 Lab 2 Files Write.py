29998a0c815cde6d4ccce38004f5d89b2b86cc48
Sagar-DU <nahidhasansagar@gmail.com>
2024-04-15 22:20:02 +0600

    This one is hard

--- Python for Data Science, AI & Development/Codes/Module 4 Lab 2 Files Write.py
+++ Python for Data Science, AI & Development/Codes/Module 4 Lab 2 Files Write.py
@@ -131,3 +131,103 @@ def genFiles (current, old):
             writefile.write(data.format(rnd(10000, 99999), date, fee[1]))
 
 genFiles (memReg, exReg)
+
+  
+'''
+The two arguments for this function are the files:
+    - currentMem: File containing list of current members
+    - exMem: File containing list of old members
+    
+    This function should remove all rows from currentMem containing 'no' 
+    in the 'Active' column and appends them to exMem.
+    '''
+def cleanFiles(currentMem, exMem):
+    # TODO: Open the currentMem file as in r+ mode
+    with open (currentMem,"r+") as writefile:
+        #TODO: Open the exMem file in a+ mode
+        with open (exMem, "a+") as appendfile:
+            # get the data 
+            writefile.seek(0)
+            members = writefile.readlines()
+            # remove header 
+            header = members[0]
+            members.pop(0)
+
+            inactive = []
+            for member in members:
+                if "no" in member:
+                    inactive.append(member)
+
+            writefile.seek(0)
+            writefile.write(header)
+
+            for member in members:
+                if (member in inactive):
+                    appendfile.write(member)
+                else:
+                    writefile.write(member)
+            writefile.truncate()
+
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+cleanFiles(memReg,exReg)
+
+
+headers = "Membership No  Date Joined  Active  \n"
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
+
+
+# Testing Pass Fail
+def testMsg(passed):
+    if passed:
+       return 'Test Passed'
+    else :
+       return 'Test Failed'
+
+testWrite = "testWrite.txt"
+testAppend = "testAppend.txt" 
+passed = True
+
+genFiles(testWrite,testAppend)
+
+with open(testWrite,'r') as file:
+    ogWrite = file.readlines()
+
+with open(testAppend,'r') as file:
+    ogAppend = file.readlines()
+
+try:
+    cleanFiles(testWrite,testAppend)
+except:
+    print('Error')
+
+with open(testWrite,'r') as file:
+    clWrite = file.readlines()
+
+with open(testAppend,'r') as file:
+    clAppend = file.readlines()
+        
+# checking if total no of rows is same, including headers
+
+if (len(ogWrite) + len(ogAppend) != len(clWrite) + len(clAppend)):
+    print("The number of rows do not add up. Make sure your final files have the same header and format.")
+    passed = False
+    
+for line in clWrite:
+    if  'no' in line:
+        passed = False
+        print("Inactive members in file")
+        break
+    else:
+        if line not in ogWrite:
+            print("Data in file does not match original file")
+            passed = False
+print ("{}".format(testMsg(passed)))
+    
+
