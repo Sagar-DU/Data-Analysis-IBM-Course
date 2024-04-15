c1586a5a747d3d75b36de8a4b5a56b5f68e7da3e
Sagar-DU <nahidhasansagar@gmail.com>
2024-04-15 20:09:10 +0600

    files are tiring. But learning something new

--- Python for Data Science, AI & Development/Codes/Module 4 Lab 2 Files Write.py
+++ Python for Data Science, AI & Development/Codes/Module 4 Lab 2 Files Write.py
@@ -103,3 +103,31 @@ with open (example2, "r") as readfile:
 with open ("Example3.txt", "r") as readfile:
     print ("This is new file data copied from previous file:")
     print (readfile.read())
+
+# Excercise
+# 1. Your local university's Raptors fan club maintains a register of its active members on a .txt document. Every month they update the file by removing the members who are not active. You have been tasked with automating this with your Python skills.
+# Given the file currentMem, Remove each member with a 'no' in their Active column. Keep track of each of the removed members and append them to the exMem file. Make sure that the format of the original files in preserved. (Hint: Do this by reading/writing whole lines and ensuring the header remains )
+# Run the code block below prior to starting the exercise. The skeleton code has been provided for you. Edit only the cleanFiles function.
+from random import randint as rnd
+memReg = "members.txt"
+exReg = "inactive.txt"
+fee = ("yes", "no")
+
+def genFiles (current, old):
+    with open (current, "w+") as writefile:
+        writefile.write("Membership No Data Joined Active \n")
+        data = "{:^13} {:<11} {:<6} \n"
+
+        for rowno in range(20):
+            date = str (rnd (2015, 2020)) + "-" + str (rnd (1, 12)) + "-" + str (rnd(1, 25))
+            writefile.write(data.format(rnd(10000, 99999), date, fee[rnd(0,1)]))
+
+    with open (old, "w+") as writefile:
+        writefile.write ("Membership No Date Joined Active \n")
+        data = "{:^13} {:<11} {:<6} \n"
+
+        for rowno in range (3):
+            date = str (rnd (2015, 2020)) + "-" + str (rnd (1, 12)) + "-" + str (rnd(1, 25))
+            writefile.write(data.format(rnd(10000, 99999), date, fee[1]))
+
+genFiles (memReg, exReg)
