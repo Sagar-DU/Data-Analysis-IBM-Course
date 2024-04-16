0842f926fbf3faf99c32385e5bd851a5a712e130
Sagar-DU <nahidhasansagar@gmail.com>
2024-04-16 22:31:06 +0600

    Module 4 Lab 3 done!

--- Python for Data Science, AI & Development/Codes/Module 4 Lab 3 Pandas.py
+++ Python for Data Science, AI & Development/Codes/Module 4 Lab 3 Pandas.py
@@ -32,26 +32,35 @@ print (df2.loc["Jane", "Salary"])
 print (df2.loc["Jane", "Department"])
 print (df2.iloc[3,2])
 
+# Slicing 
+print (df.iloc[0:2, 0:3])
+print (df.loc[0:2, "ID":"Department"])
+
+print (df2.loc["Rose":"Jane", "ID":"Department"])
 
 
-# # Excercise 
-# # Problem 1: Create a dataframe to display the result as below:
-# Data = {"Student": ["David", "Samuel", "Terry", "Evan"], "Age": [27, 24, 22, 32], "Country": ["UK", "Canada", "China", "USA"], "Course": ["Python", "Data Structures", "Machine Learning", "Web Development"], "Marks": [85, 72, 89, 75]}
 
-# dataFrame = pd.DataFrame(Data)
-# print (dataFrame)
+# Excercise 
+# Problem 1: Create a dataframe to display the result as below:
+Data = {"Student": ["David", "Samuel", "Terry", "Evan"], "Age": [27, 24, 22, 32], "Country": ["UK", "Canada", "China", "USA"], "Course": ["Python", "Data Structures", "Machine Learning", "Web Development"], "Marks": [85, 72, 89, 75]}
 
-# # Problem 2: Retrieve the Marks column and assign it to a variable b
-# b = dataFrame[["Marks"]]
-# print (b)
+dataFrame = pd.DataFrame(Data)
+print (dataFrame)
 
-# # Problem 3: Retrieve the Country and Course columns and assign it to a variable c
-# c = dataFrame[["Country", "Course"]]
-# print (c)
+# Problem 2: Retrieve the Marks column and assign it to a variable b
+b = dataFrame[["Marks"]]
+print (b)
 
-# # To view the column as a series, just use one bracket:
-# x = dataFrame ["Student"]
-# print(x)
-# print(type(x))
+# Problem 3: Retrieve the Country and Course columns and assign it to a variable c
+c = dataFrame[["Country", "Course"]]
+print (c)
 
+# To view the column as a series, just use one bracket:
+x = dataFrame ["Student"]
+print(x)
+print(type(x))
 
+# using loc() function, do slicing on old dataframe df to retrieve the Name, ID and department of index column having labels as 2,3
+print (df.loc[2:3, "Name":"Department"])
+
+# Completed
