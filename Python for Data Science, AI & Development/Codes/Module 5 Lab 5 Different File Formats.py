0cedd2b66e05243b6555cf977b7641d749eada56
Sagar-DU <nahidhasansagar@gmail.com>
2024-04-26 01:29:40 +0600

    Moudle 5 Lab 5 Different File Format started

---
+++ Python for Data Science, AI & Development/Codes/Module 5 Lab 5 Different File Formats.py
@@ -0,0 +1,4 @@
+print ("\tTution Free Days")
+print ("Sumaiya's Days: Sunday, Thursday 8:00pm")
+print ("Waffa's Days: Friday, Saturday, Sunday 2:00pm")
+print ("Fahim's Days: Monday, Thursday 6:00pm")
